605450fb92b87a00168318f425f900a37c2e43cc
Paul A Kukiel <kukielp@gmail.com>
2021-02-22 18:47:32 +1100

    Removed old Library

--- api/lambda.js
+++ api/lambda.js
@@ -1,22 +1,4 @@
-//const awsServerlessExpress = require('aws-serverless-express');
 const serverlessExpress = require('@vendia/serverless-express')
 const app = require('./app');
-
-const binaryMimeTypes = [
-    'application/octet-stream',
-    'application/xml',
-    'font/eot',
-    'font/opentype',
-    'font/otf',
-    //'image/jpeg',
-    //'image/png',
-    'image/svg+xml',
-
-
-]
-
-//const server = awsServerlessExpress.createServer(app, null)
-
-//exports.handler = (event, context, callback) => { return awsServerlessExpress.proxy(server, event, context) };
 
 exports.handler = serverlessExpress({ app })
